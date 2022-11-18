cf4b2322abbb55e93c7e2d6e7f5063dfee649c2e
hullabrian <Beierlejc@gmail.com>
2022-11-18 11:13:24 -0600

    Added sanity check and motor control

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
 
 # Create your objects here.
 ev3 = EV3Brick()
+ev3.speaker.beep() # Sanity Check
 
-
-# Write your program here.
-ev3.speaker.beep()
+motorA = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+motorA.run_time(1000, 10000)
