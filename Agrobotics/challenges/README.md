d4baa63810ede5947cc6bda9784cba3add7d1fed
hullabrian <Beierlejc@gmail.com>
2023-01-08 21:12:25 -0600

    Reworked the way that challenges are done. View the README.md in the challenges directory for details.

---
+++ Agrobotics/challenges/README.md
@@ -0,0 +1,8 @@
+# How to add a challenge
+NOTE: Make sure to have a consistent ChallengeName, otherwise this will not work.
+1. Edit challenges.csv to add the challenge to the directory, and add the line
+    ```
+    ChallengeName,location_q location_r,target_q target_r
+    ```
+2. Under the instructions directory, create a .txt file with the title of the challenges
+3. Add the robot instructions to the .txt file
--- Agrobotics/challenges/__init__.py
+++ Agrobotics/challenges/__init__.py
@@ -1,9 +1,38 @@
-class challenge(object):
-    """
-    This is the parent class for all classes/modules in the challenges directory.
-    Peta' Griffin
-    """
+from __future__ import annotations
+from dataclasses import dataclass
+import csv
+import os
 
-    def __init__(self, location: tuple, direction: tuple | str):
-        self.location = location
-        self.direction = direction  # direction represents either a direction vector or a str containing "ANY"
+
+def load_challenges() -> list[Challenge]:
+    challenges: list[Challenge] = []
+    with open("challenges.csv", "r") as file:
+        reader = csv.reader(file)
+        _ = next(reader)  # Names for each field
+
+        for line in reader:
+            challenges.append(Challenge(
+                name=line[0],
+                location=tuple(line[1].split(" ")),
+                target=tuple(line[2].split(" ")),
+                instructions=""
+            ))
+
+    names = {}
+    for instruction_file_name in os.listdir("instructions"):
+        with open(instruction_file_name, "r") as instruction_file:
+            instructions = instruction_file.readlines()
+        names[instruction_file_name.replace(".txt", "")] = instructions
+
+    for challenge in challenges:
+        challenge.instructions = names[challenge.name]
+
+    return challenges
+
+
+@dataclass
+class Challenge:
+    name: str
+    location: tuple
+    target: tuple
+    instructions: str
