56e7b29a5201bd240bed4ce02a4c239f036cb35c
hullabrian <Beierlejc@gmail.com>
2023-01-17 19:19:59 -0600

    Follow up push

---
+++ Agrobotics/out/GrowingMedium4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+left_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+robo_gun = Motor(Port.C, positive_direction=Direction.CLOCKWISE)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=82, axle_track=101)
+
+
+# ----MAIN---- #
+
