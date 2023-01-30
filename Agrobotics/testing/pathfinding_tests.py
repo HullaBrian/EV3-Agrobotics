c0ebd645c24faa05775baa50a09537d92c5b8496
hullabrian <Beierlejc@gmail.com>
2023-01-30 08:43:59 -0600

    Added a lot of unittests

--- Agrobotics/testing/pathfinding_tests.py
+++ Agrobotics/testing/pathfinding_tests.py
@@ -7,13 +7,31 @@ from Agrobotics.pathfinding.pathfinder import hexToRect
 
 class pathfinding_tests(unittest.TestCase):
     def test_coordinate_conversions(self):
-        self.assertEqual("EE", "EEe")
+        pass # self.assertEqual("EE", "EEe")
 
-    def test_distance_resolution(self):
-        self.assertEqual(858, distance_between_hexes((11, 11)))
+    def test_docking_station_distance(self):
+        self.assertEqual(900, distance_between_hexes((40 - 29, 40 - 29)))  # Docking station
 
-    def test_angle_resolution(self):
-        self.assertEqual(22, 2)
+    def test_farming_module_distance(self):
+        self.assertEqual(700, distance_between_hexes((44 - 29, 14 - 29)))  # Farming Module
+
+    def test_growing_medium_by_docking_station(self):
+        self.assertEqual(580, distance_between_hexes((39 - 29, 29 - 33)))
+
+    def test_arbitrary_distance(self):
+        self.assertEqual(175, distance_between_hexes((27 - 29, 33 - 29)))  # Arbitrary distance
+
+    def test_docking_station_angle(self):
+        self.assertEqual(0, angle_between_hexes((40 - 29, 40 - 29)))  # Docking station
+
+    def test_farming_module_angle(self):
+        self.assertEqual(90, angle_between_hexes((44 - 29, 14 - 29)))  # Farming Module
+
+    def test_growing_medium_angle(self):
+        self.assertEqual(330, angle_between_hexes((19 - 29, 49 - 29)))
+
+    def test_arbitrary_angle(self):
+        self.assertEqual(30, angle_between_hexes((32 - 29, 29 - 29)))
 
 
 if __name__ == "__main__":
